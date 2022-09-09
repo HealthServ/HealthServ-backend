f4d2b6afc6b962c8375362b0125318bf299db135
Ian M <44310933+mnai01@users.noreply.github.com>
2022-09-09 14:18:15 -0400

    Add controller ideas

--- functions/src/controller/userController.ts
+++ functions/src/controller/userController.ts
@@ -1,9 +1,32 @@
-import admin from 'firebase-admin';
+// import admin from 'firebase-admin';
 import { Request, Response } from 'firebase-functions/node_modules/@types/express';
 
-const userController = (req: Request, res: Response): any => {
+export const userInfo = (req: Request, res: Response): any => {
     // admin.firestore().collection('routines');
-    return res.status(200).send('Hey threre!');
+    return res.status(200).send('should get users basic info for homepage');
 };
 
-export default userController;
+export const userRoutines = (req: Request, res: Response): any => {
+    // admin.firestore().collection('routines');
+    return res.status(200).send('should return users routines');
+};
+
+export const userSubscribedRoutines = (req: Request, res: Response): any => {
+    // admin.firestore().collection('routines');
+    return res.status(200).send('should be able to subscribe to routines');
+};
+
+export const userRoutineProgress = (req: Request, res: Response): any => {
+    // admin.firestore().collection('routines');
+    return res.status(200).send('should be able to log progress for routines');
+};
+
+export const userRateRoutine = (req: Request, res: Response): any => {
+    // admin.firestore().collection('routines');
+    return res.status(200).send('should be used to like routines');
+};
+
+export const userCreateRoutine = (req: Request, res: Response): any => {
+    // admin.firestore().collection('routines');
+    return res.status(200).send('should be used to Create Routine');
+};
